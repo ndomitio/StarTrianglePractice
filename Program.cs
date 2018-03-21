70ae5bcc6d40d4e753577d5ad2aa4e7a00d9ce5a
NicholasDomitio <nicholas.domitio@gmail.com>
2018-03-20 22:14:42 -0400

    first

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarTrianglePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int star = 0;
+            int num = 0;
+            //as rows and stars increase by 1 and = (rows*2) +1, ending at 5.  The spaces
+            //need to decrease by 1 starting at 3 and ending at 0.
+            for (int row = 0; row < 6; row++)
+            {
+                for (int j = 0; j < (5-row); j++)
+                {
+                    Console.Write(" ");
+                }
+                for (int k = 0; k<(row*2-1); k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
+
+
+
